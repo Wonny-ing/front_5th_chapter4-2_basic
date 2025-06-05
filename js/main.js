e06347abcfe6993db66b6a879caa5aefd00cd977
Wonny <h.wonny11@gmail.com>
2025-06-06 02:32:23 +0900

    LCP 개선

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+    const country = "France";
+    const vat = 20;
+    const bar = document.querySelector("section.country-bar");
+
+    if (bar) {
+        bar.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+        bar.classList.remove("hidden");
+    }
 }
 
 
 
-showTopBar();
+document.addEventListener("DOMContentLoaded", showTopBar);
 
 
 
